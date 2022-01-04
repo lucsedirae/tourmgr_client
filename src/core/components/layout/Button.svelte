1b9ad1c217566b0a39a2040cea42b01af4532f6d
Jon Deavers <jondeavers@gmail.com>
2022-01-04 01:35:22 -0500

    FEAT1 Added button. Refactored CSS

---
+++ src/core/components/layout/Button.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<button class="btn" ><slot /></button>
+
+<style>
+  .btn {
+    color: var(--tertiary-light);
+    border: 0;
+    border-radius: 8px;
+    background-color: var(--primary);
+    width: 100px;
+    height: 40px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    transform: scale(0.98);
+    background-color: var(--primary-dark);
+  }
+</style>
--- src/core/components/pages/Home.svelte
+++ src/core/components/pages/Home.svelte
@@ -1,7 +1,18 @@
 <script>
-  import {str_welcome_msg} from '../../../config/lang/template.json'
+  import { str_welcome_msg } from '../../../config/lang/template.json';
+  import { str_register } from '../../../config/lang/lang.json';
+
+  import Button from '../layout/Button.svelte';
 </script>
 
 <div class="container">
-  <p class="text-center">{str_welcome_msg}</p>
+  <div>
+    <p class="text-center">{str_welcome_msg}</p>
+  </div>
+
+  <div class="break" />
+
+  <div>
+    <Button>{str_register}</Button>
+  </div>
 </div>
