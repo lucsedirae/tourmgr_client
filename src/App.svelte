5c906a11da04177908b9b43f42ed182ff79d5baf
Jon Deavers <jondeavers@gmail.com>
2022-01-16 23:53:02 -0500

    Routing init

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,21 @@
 <script>
-import Layout from './core/components/layout/Layout.svelte'
+  import { Router, Link, Route } from 'svelte-routing';
+
+  import About from './core/components/layout/About.svelte';
+	import Login from './core/components/user/Login.svelte'
+  import Navbar from './core/components/layout/Navbar.svelte';
+  import Register from './core/components/user/Register.svelte';
+
+  export let url = '';
 </script>
 
-<main>
-	<Layout />
-</main>
-
+<Router {url}>
+  <main>
+    <Navbar />
+		<div>
+			<Route path="register" component={Register} />
+			<Route path="login" component={Login} />
+			<Route path="/"><About /></Route>
+		</div>
+	</main>
+</Router>
--- src/core/components/layout/Layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import {siteName} from '../../../config/site/siteconfig.json'
-</script>
-
-<div>
-  <h1>{siteName}</h1>
-  <slot></slot>
-</div>
